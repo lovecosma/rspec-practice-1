61d8cb50343ec51a82d6ba0cbcbd440736e44302
love.cosma <williamsmatthew333@gmail.com>
2021-11-02 22:38:53 -0600

    added two more tests and coded solution.

--- lib/median.rb
+++ lib/median.rb
@@ -1,14 +1,63 @@
-class Median 
+def median_of_two_sorted_arrays_with_different_lengths array1, array2
 
-        attr_accessor :number, :arr1, :arr2
-    
-    def initialize(number = 1)
-        @number = number
+
+    n = array1.length
+    m = array2.length
+
+    m1 = -1 
+    m2 = -1
+
+    i = 0
+    j = 0
+
+    counter = 0
+
+    return 'no median here' if n == 0 && m == 0
+    if (n+m) % 2 == 1  
+        while counter <= (n+m)/2
+            if i != n && j != m
+                if array1[i] < array2[j] 
+                    m1 = array1[i]
+                    i += 1
+                else
+                    m1 = array2[j]
+                    j += 1
+                end 
+            elsif i < n
+                m1 = array1[i]
+                i += 1
+            else
+                m1 = array2[j]
+                j += 1 
+            end  
+            counter += 1
+        end 
+        return m1
+    else 
+        while counter <= (n+m)/2
+            m2 = m1
+            if i != n && j != m
+                if array1[i] < array2[j] 
+                    m1 = array1[i]
+                    i += 1
+                else
+                    m1 = array2[j]
+                    j += 1
+                end 
+            elsif i < n
+                m1 = array1[i]
+                i += 1
+            else
+                m1 = array2[j]
+                j += 1 
+            end  
+            counter += 1
+        end 
+        return ((m2.to_f + m1.to_f) / 2)
+
     end 
 
-    def two_sorted_arrays_with_different_lengths(arr1, arr2)
-        @arr1 = arr1
-        @arr2 = arr2
-    end 
+
+
 
 end 
--- spec/median_spec.rb
+++ spec/median_spec.rb
@@ -1,13 +1,28 @@
 
-RSpec.describe Median do 
+describe "#median_of_two_sorted_arrays_with_different_lengths" do 
 
-    it "can be instantiated" do 
-        expect(Median.new).not_to be_nil
+    it "should calculate the median of two sorted arrays with different lengths when the combined length of the two arrays is odd" do
+       arr1 = [1,3]
+       arr2 = [2]
+       expect(median_of_two_sorted_arrays_with_different_lengths arr1, arr2).to eq(2)
     end 
-    it "can be instantiated with two arrays" do 
-        arr1 = [1,2,3]
-        arr2 = [5,6,7]
-        median = Median.new(arr1, arr2)
-    end 
+
+    it "should calculate the median of two sorted arrays with different lengths when the combined length of the two arrays is even" do
+        arr1 = [1,3]
+        arr2 = [2,5]
+        expect(median_of_two_sorted_arrays_with_different_lengths arr1, arr2).to be_within(0.01).of(2.5)
+     end 
+
+     it "can handle an empty array" do 
+        arr1 = [1,3]
+        arr2 = []
+        expect(median_of_two_sorted_arrays_with_different_lengths arr1, arr2).to be_within(0.01).of(2.0)
+     end 
+
+     it "returns 'no median here' for two empty arrays" do 
+        arr1 = []
+        arr2 = []
+        expect(median_of_two_sorted_arrays_with_different_lengths arr1, arr2).to eq 'no median here'
+     end 
 
 end 
